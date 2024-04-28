73dcadabd121a5beaf20d63576e1743969ca067e
Soeren Wegener <wegener92@gmail.com>
2024-04-28 16:05:51 +0200

    Add GH actions for publishing at gh package registry

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Build the project
+wasm-pack build
+
+# Modify package.json to fit our needs
+# https://github.com/rustwasm/wasm-pack/issues/427#issuecomment-458180179
+
+PACKAGE_JSON=$(cat pkg/package.json)
+
+# Namespace the package to be able to publish to GitHub Packages / brief-jetzt organization
+PACKAGE_JSON=$(echo $PACKAGE_JSON | jq '.["name"] = "@brief-jetzt/wasm-typst"')
+
+# Adjust the publishConfig
+PACKAGE_JSON=$(echo $PACKAGE_JSON | jq '.["publishConfig"] = {"registry": "https://npm.pkg.github.com/"}')
+
+# Add the repository field
+PACKAGE_JSON=$(echo $PACKAGE_JSON | jq '.["repository"] = "https://github.com/brief-jetzt/wasm-typst"')
+
+
+echo $PACKAGE_JSON | jq
+#echo $PACKAGE_JSON > pkg/package.json
