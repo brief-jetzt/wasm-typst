a8ff4c63bfacec5e971d41ff48f480c66f9bf350
Soeren Wegener <wegener92@gmail.com>
2024-04-28 22:16:11 +0200

    Revert "Explicit package.json"
    This reverts commit 2aab3cf6b85bec20bc70eef3947ac39befd00001.

--- build.sh
+++ build.sh
@@ -3,4 +3,19 @@
 set -euo pipefail
 
 # Build the project
-wasm-pack build -d js-src/wasm-pkg
+wasm-pack build
+
+# Modify package.json to fit our needs
+# https://github.com/rustwasm/wasm-pack/issues/427#issuecomment-458180179
+
+PACKAGE_JSON=$(cat pkg/package.json)
+
+# Namespace the package to be able to publish to GitHub Packages / brief-jetzt organization
+PACKAGE_JSON=$(echo "$PACKAGE_JSON" | jq '.["name"] = "@brief-jetzt/wasm-typst"')
+
+# Adjust the publishConfig
+PACKAGE_JSON=$(echo "$PACKAGE_JSON" | jq '.["publishConfig"] = {"access": "public"}')
+
+
+echo "$PACKAGE_JSON" | jq
+echo "$PACKAGE_JSON" > pkg/package.json
